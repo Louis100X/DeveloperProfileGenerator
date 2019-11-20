223378a8d02c90853d838e0bcea9d86e6f68ed4f
Louis Alonzo <louis.a@live.com>
2019-11-19 23:54:07 -0800

    restructure init() function

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const inquirer = require("inquirer");
 const axios = require("axios");
 const generateHTML = require("./generateHTML");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
 
 const questions = [
 
@@ -11,32 +16,52 @@ function writeToFile(fileName, data) {
 }
 
 function init() {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                name: "gitHub",
-                message: "What is your gitHub Username",
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "gitHub",
+            message: "What is your gitHub Username",
+        },
+        {
+            type: "list",
+            name: "color",
+            message: "What is your favorite color?",
+            choices: [
+                "red", "blue", "green", "yellow",
+            ]
+        }
+    ])};
 
-            },
-            {
-                type: "list",
-                name: "color",
-                message: "What is your favorite color?",
-                choices: [
-                    "red", "blue", "green"
-                ]
-            }
-        ]).then(function (response) {
-            console.log(response)
-            const queryUrl = `https://api.github.com/users/${response.gitHub}`;
-            axios.get(queryUrl).then(function(gitData){
-                console.log(gitData.data);
-                gitData.data.color = response.color;
-                const hTML = generateHTML(gitData.data);
-                console.log(hTML);
-            })
+    function gitAPI(response) {
+        console.log(response)
+        const queryUrl = `https://api.github.com/users/${response.gitHub}`;
+        return axios.get(queryUrl).then(function (gitData) {
+            console.log(gitData.data);
+            gitData.data.color = response.color;
+            const hTML = generateHTML(gitData.data);
+            console.log(hTML);
+            console.log(gitData.data.login);
+            console.log(gitData.data.location);
+            console.log(gitData.data.blog);
+            console.log(gitData.data.html_url);
+            console.log(gitData.data.bio);
+            console.log(gitData.data.public_repos);
+            console.log(gitData.data.followers);
+            console.log(gitData.data.following);
         });
-}
+    };
 
-init();
+init()
+.then(function(answers) {
+    var response = init();
+    gitAPI(response);
+  const html = generateHTML(answers);
+
+  return writeFileAsync("index.html", html);
+})
+.then(function() {
+  console.log("Successfully wrote to index.html");
+})
+.catch(function(err) {
+  console.log(err);
+});;
